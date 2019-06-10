4ebf13159b454add184f63e635da3f32295b8efb
Silent Dev <rez_a@outlook.com>
2019-06-10 16:26:11 -0400

    Completed the setup to Heroku.

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://User:Password994@ds135217.mlab.com:35217/heroku_l1fw1rvb";
 
 mongoose.connect(MONGODB_URI);
